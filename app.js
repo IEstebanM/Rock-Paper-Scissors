1379931ff2d1ffdc925ee6c82bb68233b8099930
IEstebanM <90185600+IEstebanM@users.noreply.github.com>
2021-09-20 08:35:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+    /*
 	Author: Ibor Esteban - https://github.com/IEstebanM
 	This game has been made as a result of an exercise of the odin project ( https://www.theodinproject.com ).
 	License: CC BY-ND This license lets others reuse the work for any purpose, including commercially;
